45f3e41eed98fee9b65ab63ec911d2ea2760847f
mohsink13 <mohsink13@gmail.com>
2016-09-15 15:54:24 +0100

    initial commit

---
+++ SalesForceIntegrationPoC/SupportCase.UI/Controllers/CaseController.cs
@@ -0,0 +1,37 @@
+﻿using SupportCase.UI.Models;
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupportCase.UI.Controllers
+{
+    public class CaseController : Controller
+    {
+        // GET: Case
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void CreateCase(Case model)
+        {
+            foreach (string file in Request.Files)
+            {
+                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+                if (hpf.ContentLength == 0)
+                    continue;
+                string savedFileName = Path.Combine(
+                   AppDomain.CurrentDomain.BaseDirectory,
+                   Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+                
+            }
+
+            Response.RedirectToRoute(new { controller = "case", action = "index" });
+        }
+    }
+
+    
+}
---
+++ SalesForceIntegrationPoC/SupportCase.UI/Models/Case.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupportCase.UI.Models
+{
+    public class Case
+    {
+        public string txtCompanyName { get; set; }
+        public string txtForename { get; set; }
+        public string txtSurname { get; set; }
+        public string txtTelephone { get; set; }
+        public string txtEmail { get; set; }
+        public int cboproduct { get; set; }
+        public int txtVersion { get; set; }
+        public string txtProblem { get; set; }
+        public int cbowindows { get; set; }
+        public int cbonetwork { get; set; }
+    }
+}
