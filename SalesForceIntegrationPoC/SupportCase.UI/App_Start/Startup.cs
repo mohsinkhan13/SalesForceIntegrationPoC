2280432ae9062df9ccef2018d2fb39e3a37f2443
mohsink13 <mohsink13@gmail.com>
2016-09-16 16:15:15 +0100

    added some authentication code

---
+++ SalesForceIntegrationPoC/SupportCase.UI/App_Start/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
+using System.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.ServiceModel.Security.Tokens;
+
+[assembly: OwinStartup(typeof(SupportCase.UI.App_Start.Startup))]
+
+namespace SupportCase.UI.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap
+                = new Dictionary<string, string>();
+
+
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            //});
+
+            //app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            //{
+            //    ClientId = "portal",
+            //    Authority = "https://services.oneclickuk.de/authority/",
+            //    RedirectUri = "http://localhost:49525/",
+            //    ResponseType = "id_token token",
+            //    Scope = "oneclick",
+            //    PostLogoutRedirectUri = "http://localhost:49525/",
+            //    SignInAsAuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+
+            //    Notifications = new OpenIdConnectAuthenticationNotifications
+            //    {
+            //        SecurityTokenValidated = async n =>
+            //        {
+            //            var nid = new ClaimsIdentity(
+            //                n.AuthenticationTicket.Identity.AuthenticationType,
+            //                IdentityServer3.Core.Constants.ClaimTypes.GivenName,
+            //                IdentityServer3.Core.Constants.ClaimTypes.Role);
+
+            //            // get userinfo data
+            //            var userInfoClient = new UserInfoClient(
+            //                new Uri(n.Options.Authority + "/connect/userinfo"),
+            //                n.ProtocolMessage.AccessToken);
+
+            //            var userInfo = await userInfoClient.GetAsync();
+            //            userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+
+            //            // keep the id_token for logout
+            //            nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+            //            // add access token for sample API
+            //            nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+
+            //            // keep track of access token expiration
+            //            nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+
+            //            // add some other app specific claim
+            //            nid.AddClaim(new Claim("app_specific", "some data"));
+
+            //            n.AuthenticationTicket = new AuthenticationTicket(
+            //                nid,
+            //                n.AuthenticationTicket.Properties);
+            //        },
+
+            //        RedirectToIdentityProvider = n =>
+            //        {
+            //            if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+            //            {
+            //                var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+            //                if (idTokenHint != null)
+            //                {
+            //                    n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+            //                }
+            //            }
+
+            //            return Task.FromResult(0);
+            //        }
+            //    }
+            //});
+        }
+    }
+}
--- SalesForceIntegrationPoC/SupportCase.UI/Controllers/CaseController.cs
+++ SalesForceIntegrationPoC/SupportCase.UI/Controllers/CaseController.cs
@@ -1,6 +1,9 @@
-﻿using SupportCase.UI.Models;
+﻿using IdentityModel.Client;
+using SupportCase.UI.Models;
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +12,12 @@ namespace SupportCase.UI.Controllers
     public class CaseController : Controller
     {
         // GET: Case
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var response = await GetTokenAsync();
+            //var result = await CallApi(response.AccessToken);
+
+            return Content(response.HttpErrorReason);
         }
 
         [HttpPost]
@@ -30,6 +36,25 @@ namespace SupportCase.UI.Controllers
             }
 
             Response.RedirectToRoute(new { controller = "case", action = "index" });
+        }
+
+        private async Task<string> CallApi(string token)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(token);
+            var result = await client.GetStringAsync("https://eu11.salesforce.com/services/data/v37.0/sobjects/case/");
+
+            return result;
+        }
+
+        private async Task<TokenResponse> GetTokenAsync()
+        {
+            var client = new TokenClient(
+                "https://eu11.salesforce.com/services/oauth2/token",
+                "3MVG9HxRZv05HarTSJgKRE.cSe2bXxWHSGRRRorSvgPdndC3ij7Rh_ybeToHZ_3IDOnjZeUNLFiM5X4UICdeU",
+                "6282425504841271049");
+
+            return await client.RequestResourceOwnerPasswordAsync("mohsink13@gmail.com", "Salesf0rceK08H3JHxrYXz33N9T73cX7ld");
         }
     }
 
